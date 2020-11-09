10d888c3bb0541ebe3eb4b857f6b5b2ff25c032d
natachaiit <natachaiit.nata@gmail.com>
2020-11-10 02:59:56 +0700

    Update Error redit

--- api/libs/redis.js
+++ api/libs/redis.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const redis = require('redis');
 const { promisify } = require('util');
 const clientredis = redis.createClient({
-    host: process.env.REDIS_HORT || '127.0.0.1',
+    host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || '6379'
 });
 const getAsync = promisify(clientredis.get).bind(clientredis)
